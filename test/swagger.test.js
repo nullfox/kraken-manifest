eefeddd72665f684b866d1f9467cc97061d4432f
befox <ben@nullfox.com>
2016-04-26 00:10:38 +0000

    Updates tests

--- test/swagger.test.js
+++ test/swagger.test.js
@@ -1,13 +1,29 @@
 /* global define, it, describe, context, expect, sinon */
 
-import Client from '../src/client';
+import Path from 'path';
+import YAML from 'yamljs';
+import Swagger from '../src/swagger';
 
-describe('Client', () => {
-  context('when instanciated', () => {
-    it('should setup a ZMQ dealer socket', () => {
-      const client = new Client();
+const kraken = YAML.load(Path.join(__dirname, './fixtures/.kraken'));
 
-      expect(client.socket.type).to.be.equal('dealer');
+describe('Swagger', () => {
+  context('when instanciated with a string path', () => {
+    it('should load the file', () => {
+      const swagger = new Swagger(Path.join(__dirname, './fixtures/.kraken'));
+
+      expect(swagger.kraken).to.be.a('object');
+      expect(swagger.kraken).to.have.property('title');
+      expect(swagger.kraken.title).to.be.equal(kraken.title);
+    });
+  });
+
+  context('when instanciated with an object', () => {
+    it('should load the object', () => {
+      const swagger = new Swagger(kraken);
+
+      expect(swagger.kraken).to.be.a('object');
+      expect(swagger.kraken).to.have.property('title');
+      expect(swagger.kraken.title).to.be.equal(kraken.title);
     });
   });
 });
