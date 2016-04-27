84011906a2f4142694491442c5089a2b91bd453f
befox <ben@nullfox.com>
2016-04-27 20:16:20 +0000

    Removed test

--- test.js
+++
@@ -1,8 +0,0 @@
-import Swagger from './src/swagger';
-
-const swagger = new Swagger('./test/fixtures/.kraken');
-swagger.generateAndWrite('./swagger.json', {
-  examples: false,
-  validators: false,
-  apiGateway: true
-});
